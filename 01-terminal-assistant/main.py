2a1700a32449ed8e33a09872095fca6a80aec002
Aurimas <aurimasnarkevicius@gmail.com>
2026-04-15 18:41:39 +0200

    Used local memory fo the conversation transcript.

--- 01-terminal-assistant/main.py
+++ 01-terminal-assistant/main.py
@@ -11,11 +11,7 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 TRANSCRIPT_PATH = "01-terminal-assistant/transcript.json"
-STATE_PATH = "01-terminal-assistant/state.json"
-previous_response_id = None
 
-print("Terminal Assistant")
-print("Type 'exit' to quit./n")
 
 if os.path.exists(TRANSCRIPT_PATH):
     with open(TRANSCRIPT_PATH, 'r') as f:
@@ -24,13 +20,8 @@ if os.path.exists(TRANSCRIPT_PATH):
 else:
     transcript = []
 
-if os.path.exists(STATE_PATH):
-    with open(STATE_PATH, "r") as f:
-        state = json.load(f)
-    previous_response_id = state.get("previous_response_id")
-    print(f"Loaded previous_response_id: {previous_response_id}")
-else:
-    previous_response_id = None
+print("Terminal Assistant")
+print("Type 'exit' to quit./n")
 
 
 while True:
@@ -40,35 +31,27 @@ while True:
         with open(TRANSCRIPT_PATH, "w") as f:
             json.dump(transcript, f, indent=2)
         print(f"Transcript saved to {TRANSCRIPT_PATH}")
-
-        with open(STATE_PATH, "w") as f:
-            json.dump({"previous_response_id": previous_response_id}, f, indent=2)
-        print(f"State saved to {STATE_PATH}")
-
-
         print("Goodbye!")
         break
 
     if not user_input:
         continue
     
-    transcript.append({"role":"user","content":user_input})
+    new_user_message = {
+        "role":"user",
+        "content":user_input,
+    }
 
-    request = {
-        "model" : "gpt-4o-mini",
-        "instructions" : "You are a helpful programming tutor. Be concise, practical, and clear.",
-        "input" : user_input,
-        }
+    model_input = transcript + [new_user_message]
     
-
-    if previous_response_id:
-        request["previous_response_id"] = previous_response_id
-
     print(f"\nAssistant: ", end="",flush=True)
-
     assistant_text = ""
 
-    with client.responses.stream(**request) as stream:
+    with client.responses.stream(
+        model="gpt-4o-mini",
+        instructions="You are a helpful Python instructor. Be concice, clear and pracitcal.",
+        input=model_input
+    ) as stream:
         for event in stream:
             if event.type == "response.output_text.delta":
                 print(event.delta, end="", flush=True)
@@ -76,7 +59,7 @@ while True:
         
         final_response = stream.get_final_response()
 
+    print("\n")
     transcript.append({"role": "assistant",
                        "content": assistant_text})
-    previous_response_id = final_response.id
    
